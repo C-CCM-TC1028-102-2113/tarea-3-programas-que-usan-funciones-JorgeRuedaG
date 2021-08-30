290d08c168a6f1f65299db94aec84a598fd059b3
JorgeRuedaG <89436064+JorgeRuedaG@users.noreply.github.com>
2021-08-29 20:54:39 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,6 +1,13 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    base=float(input("Dame la base:"))
+    alt=float(input("Dame la altura:"))
+    area=base*alt
+    prof=float(input("Dame la profundidad:"))
+    vol=area*prof
+    print("el volumen es:" + str(vol))
+    
     pass
 
 if __name__=='__main__':
